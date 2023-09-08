04a5d7cc49d31b3c0eb5dfc6b84fdf98826910b2
Shane Lyu <alrope@cs.washington.edu>
2023-09-07 17:15:44 -0700

    updated commands

--- commands.sh
+++ commands.sh
@@ -1,22 +1,65 @@
+# target/release/bff \
+#   --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_0.bff  \
+#   --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_0 \
+#   --no-update-bloom-filter \
+#   --reader-mode 2 \
+#   --writer-mode 2 \
+#   --bloom-filter-size 1000000000 \
+#   --expected-ngram-count 390794801 \
+#   --annotate-attribute-only \
+#   /gscratch/h2lab/alrope/data/redpajama/arxiv/*.jsonl
+
+# target/release/bff \
+#   --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_1.bff  \
+#   --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_1 \
+#   --no-update-bloom-filter \
+#   --reader-mode 2 \
+#   --writer-mode 2 \
+#   --bloom-filter-size 1000000000 \
+#   --expected-ngram-count 390794801 \
+#   --annotate-attribute-only \
+#   /gscratch/h2lab/alrope/data/redpajama/arxiv/*.jsonl
+
 target/release/bff \
   --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_0.bff  \
-  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_0 \
+  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_0/AA \
   --no-update-bloom-filter \
   --reader-mode 2 \
   --writer-mode 2 \
   --bloom-filter-size 1000000000 \
   --expected-ngram-count 390794801 \
   --annotate-attribute-only \
-  /gscratch/h2lab/alrope/data/redpajama/arxiv/*.jsonl
+  /gscratch/h2lab/alrope/data/wikipedia/processed/AA/*
+
+target/release/bff \
+  --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_0.bff  \
+  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_0/AB \
+  --no-update-bloom-filter \
+  --reader-mode 2 \
+  --writer-mode 2 \
+  --bloom-filter-size 1000000000 \
+  --expected-ngram-count 390794801 \
+  --annotate-attribute-only \
+  /gscratch/h2lab/alrope/data/wikipedia/processed/AB/*
 
 target/release/bff \
   --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_1.bff  \
-  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_1 \
+  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_1/AA \
   --no-update-bloom-filter \
   --reader-mode 2 \
   --writer-mode 2 \
   --bloom-filter-size 1000000000 \
   --expected-ngram-count 390794801 \
   --annotate-attribute-only \
-  /gscratch/h2lab/alrope/data/redpajama/arxiv/*.jsonl
-  
+  /gscratch/h2lab/alrope/data/wikipedia/processed/AA/*
+
+target/release/bff \
+  --bloom-filter-file /gscratch/h2lab/micdun/bff/full_pile_train_filter_1.bff  \
+  --output-directory /gscratch/h2lab/alrope/data/bff/redpajama-arxiv+pile/filter_1/AB \
+  --no-update-bloom-filter \
+  --reader-mode 2 \
+  --writer-mode 2 \
+  --bloom-filter-size 1000000000 \
+  --expected-ngram-count 390794801 \
+  --annotate-attribute-only \
+  /gscratch/h2lab/alrope/data/wikipedia/processed/AB/*
