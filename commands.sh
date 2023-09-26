83dfec0692301d8be2a6e4e35d37d7577f445a55
Shane Lyu <alrope@uw.edu>
2023-09-26 15:00:10 -0700

    edited commands

--- commands.sh
+++ commands.sh
@@ -92,6 +92,28 @@
 #   /gscratch/h2lab/alrope/data/redpajama/arxiv_newline_removed/*.jsonl
 
 # Books3
+# target/release/bff \
+#   --bloom-filter-file /gscratch/h2lab/micdun/bff/filters/ngram_13/full_pile_train_filter_0.bff  \
+#   --output-directory /gscratch/h2lab/alrope/data/bff/books3+pile/filter_0 \
+#   --no-update-bloom-filter \
+#   --reader-mode 2 \
+#   --writer-mode 2 \
+#   --bloom-filter-size 1000000000 \
+#   --expected-ngram-count 390794801 \
+#   --annotate-attribute-only \
+#   /gscratch/h2lab/sewon/data/books3/shards/*/*.text
+
+# target/release/bff \
+#   --bloom-filter-file /gscratch/h2lab/micdun/bff/filters/ngram_13/full_pile_train_filter_1.bff  \
+#   --output-directory /gscratch/h2lab/alrope/data/bff/books3+pile/filter_1 \
+#   --no-update-bloom-filter \
+#   --reader-mode 2 \
+#   --writer-mode 2 \
+#   --bloom-filter-size 1000000000 \
+#   --expected-ngram-count 390794801 \
+#   --annotate-attribute-only \
+#   /gscratch/h2lab/sewon/data/books3/shards/*/*.text
+
 target/release/bff \
   --bloom-filter-file /gscratch/h2lab/micdun/bff/filters/ngram_13/full_pile_train_filter_0.bff  \
   --output-directory /gscratch/h2lab/alrope/data/bff/books3+pile/filter_0 \
@@ -101,7 +123,7 @@ target/release/bff \
   --bloom-filter-size 1000000000 \
   --expected-ngram-count 390794801 \
   --annotate-attribute-only \
-  /gscratch/h2lab/sewon/data/books3/shards/*/*.text
+  /gscratch/h2lab/alrope/data/redpajama/book/book.jsonl
 
 target/release/bff \
   --bloom-filter-file /gscratch/h2lab/micdun/bff/filters/ngram_13/full_pile_train_filter_1.bff  \
@@ -112,4 +134,4 @@ target/release/bff \
   --bloom-filter-size 1000000000 \
   --expected-ngram-count 390794801 \
   --annotate-attribute-only \
-  /gscratch/h2lab/sewon/data/books3/shards/*/*.text
+  /gscratch/h2lab/alrope/data/redpajama/book/book.jsonl
